f9314939ed87ad182334dca90fe4ae4ae8c0f0f3
KaseyCantu <kpc.software.engineer@gmail.com>
2021-07-07 16:16:08 -0500

    Acceptance criteria per DX-995 - test track by packageId - https://auctane.atlassian.net/browse/DX-995

--- test/package/track_package_test.rb
+++ test/package/track_package_test.rb
@@ -40,14 +40,14 @@ describe "track package" do
     )
 
     # The shipmentId is populated,, is not nil, and is a string.
-    assert result.shipment.shipment_id != nil
+    assert !result.shipment.shipment_id.nil?
 
     # The carrier_id is populated, is not nil, and is a string.
-    assert result.shipment.carrier_id != nil, "The carrier_id is populated."
+    assert !result.shipment.carrier_id.nil?, "The carrier_id is populated."
     assert result.shipment.carrier_id.is_a?(String)
 
     # The tracking_number is populated, is not nil, and is a string.
-    assert result.package.tracking_number != nil
+    assert !result.package.tracking_number.nil?
     assert result.package.tracking_number.is_a?(String)
   end
 end
