ba6ec9c99a92f6f8e91221128d640fc824601b25
KaseyCantu <kpc.software.engineer@gmail.com>
2021-07-12 10:33:40 -0500

    Added rbdoc strings

--- lib/shipengine/utils/pretty_print.rb
+++ lib/shipengine/utils/pretty_print.rb
@@ -5,10 +5,19 @@ require "json"
 module ShipEngine
   module Utils
     module PrettyPrint
+
+      # This will be used to add a *to_s* method override
+      # to each class that *includes* the *PrettyPrint* module.
+      # This method returns a *JSON String* so one can easily inspect
+      # the contents of a given object.
       def to_s
         JSON.pretty_generate(to_hash)
       end
 
+      # This will be used to add a *to_hash* method override
+      # to each class that *includes* the *PrettyPrint* module.
+      # This will return the class attributes and their values in
+      # a *Hash*.
       def to_hash
         hash = {}
         instance_variables.each do |n|
