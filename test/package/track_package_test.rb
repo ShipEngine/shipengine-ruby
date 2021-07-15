af75742d4a13ec6a69e7856a1694afe5317937ef
KaseyCantu <kpc.software.engineer@gmail.com>
2021-07-15 11:37:13 -0500

    AC per DX-1009 - Test invalid package_id prefix

--- test/package/track_package_test.rb
+++ test/package/track_package_test.rb
@@ -213,10 +213,22 @@ describe "track package" do
       source: "carrier",
       type: "business_rules",
       code: "invalid_identifier",
-      url: nil,
     }
     assert_raises_shipengine(::ShipEngine::Exceptions::BusinessRulesError, expected_err) do
       client.track_package_by_tracking_number(tracking_number, carrier_code)
+    end
+  end
+
+  it "DX-1009 - Test invalid package_id prefix" do
+    package_id = "pkg_12!@3a s567"
+    expected_err = {
+      request_id: :__REGEX_MATCH__,
+      source: "shipengine",
+      type: "validation",
+      code: "invalid_field_value",
+    }
+    assert_raises_shipengine(::ShipEngine::Exceptions::ValidationError, expected_err) do
+      client.track_package_by_id(package_id)
     end
   end
 
